61a9c70d3b91fa81ca25487bd86a30818e19169c
exploit-nato <106706622+exploit-nato@users.noreply.github.com>
2024-06-17 23:57:38 -0500

    just being goofy lmao

--- main.cpp
+++ main.cpp
@@ -1,18 +1,33 @@
 #include <iostream>
-#include <stdlib.h>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+#include <vector>
 
-void Roll() {
-    int number = (rand() % 6) + 1;
-    std::cout << "You rolled a " << number << std::endl;
+const size_t DIE_SIDES = 6;
+
+void Roll(size_t count, std::vector<size_t>& numbersRolled) {
+    size_t number = (rand() % DIE_SIDES) + 1;
+    numbersRolled.push_back(number);
+    std::cout << "Roll #" << std::setw(2) << count << ": You rolled a " << number << '\n';
 }
 
-int main() {
-    int times;
-    std::cout << "Times to roll:";
+int main(void) {
+    srand(static_cast<unsigned int>(time(NULL)));
+    size_t times = 0;
+    std::cout << "Times to roll: ";
     std::cin >> times;
-    for (int i = 0; i<times; i++) {
-        Roll();
+
+    if (times <= 0) {
+        std::cerr << "Invalid number of times to roll. Exiting...\n";
+        return 1;
     }
-    std::cin >> times;
+
+    std::vector<size_t> numbersRolled;
+    for (size_t i = 0; i < times; ++i) Roll(i + 1, numbersRolled);
+
+    std::cout << "Press any key to exit...";
+    std::cin.ignore();
+    std::cin.get();
     return 0;
 }
